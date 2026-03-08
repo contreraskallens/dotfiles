68403781280c3401bd070801d008439fe2e75c2d
Pablo Contreras Kallens <omfgzell@hostv6.entelchile.net>
2026-03-08 18:11:39 -0400

    Added bash, konsave, updated kde

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     set -gx EDITOR 'nvim'
     set -gx ZK_NOTEBOOK_DIR '/home/omfgzell/Documents/notes'
 end
+fish_add_path /home/omfgzell/AppImages
+alias filen="filen.appimage"
+alias winboat="winboat.appimage"
