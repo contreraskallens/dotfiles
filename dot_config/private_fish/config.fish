0c0b656a807b2ef76ee74f8c8b86ff00aca64ff5
Pablo Contreras Kallens <omfgzell@hostv6.entelchile.net>
2026-03-12 17:38:52 -0300

    Updated to use homebrew and minimize layering

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,3 +7,9 @@ fish_add_path /home/omfgzell/AppImages
 fish_add_path /home/omfgzell/.local/share/nvim/mason/bin
 alias filen="filen.appimage"
 alias winboat="winboat.appimage"
+set --global --export HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+set --global --export HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+set --global --export HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+fish_add_path --global --move --path "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin";
+if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
+if not contains "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH; set --global --export INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH; end;
